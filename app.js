aea4f4f7b0275578e904e04c371c499d68a95eda
Aileen C <ailanigc9@gmail.com>
2019-04-22 13:50:14 -0500

    Retrieve a list of all users

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api/users', apiusersRouter);
+app.use('/api/users', apiUsersRouter);
 
 //Connect to MongoDB
 mongoose.connect(config.mongodb, { useNewUrlParser: true });
